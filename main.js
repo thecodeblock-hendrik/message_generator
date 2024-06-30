579ea3abe5ee699cc5601f34a8a61ac70a739aed
thecodeblock.hendrik <thecodeblock.dev@gmail.com>
2024-06-30 16:00:28 +0200

    add love functionality to generator

--- main.js
+++ main.js
@@ -3,24 +3,24 @@
 const devotions = {
   'bible verses': {
 
-      love: [
-        'For this is how God loved the world: He gave his one and only Son, so that everyone who believes in him will not perish but have eternal life. - John 3:16',
-        'But God showed his great love for us by sending Christ to die for us while we were still sinners. - Romans 5:8',
-        'Love is patient and kind. Love is not jealous or boastful or proud. - 1 Corinthians 13:4'
-      ],
+    love: [
+      'For this is how God loved the world: He gave his one and only Son, so that everyone who believes in him will not perish but have eternal life. - John 3:16',
+      'But God showed his great love for us by sending Christ to die for us while we were still sinners. - Romans 5:8',
+      'Love is patient and kind. Love is not jealous or boastful or proud. - 1 Corinthians 13:4'
+    ],
 
-      hope: [
-        'For I know the plans I have for you,” says the LORD. “They are plans for good and not for disaster, to give you a future and a hope. - Jeremiah 29:11',
-        'And this hope will not lead to disappointment. For we know how dearly God loves us, because he has given us the Holy Spirit to fill our hearts with his love. - Romans 5:5',
-        'Faith shows the reality of what we hope for; it is the evidence of things we cannot see. - Hebrews 11:1'
-      ],
+    hope: [
+      'For I know the plans I have for you,” says the LORD. “They are plans for good and not for disaster, to give you a future and a hope. - Jeremiah 29:11',
+      'And this hope will not lead to disappointment. For we know how dearly God loves us, because he has given us the Holy Spirit to fill our hearts with his love. - Romans 5:5',
+      'Faith shows the reality of what we hope for; it is the evidence of things we cannot see. - Hebrews 11:1'
+    ],
 
-      fear: [
-        'For God has not given us a spirit of fear and timidity, but of power, love, and self-discipline. - 2 Timothy 1:7',
-        'Dont worry about anything; instead, pray about everything. Tell God what you need, and thank him for all he has done. - Philippians 4:6',
-        'But when I am afraid, I will put my trust in you. - Psalms 56:2'
-      ]
-    },
+    fear: [
+      'For God has not given us a spirit of fear and timidity, but of power, love, and self-discipline. - 2 Timothy 1:7',
+      'Don\'t worry about anything; instead, pray about everything. Tell God what you need, and thank him for all he has done. - Philippians 4:6',
+      'But when I am afraid, I will put my trust in you. - Psalms 56:2'
+    ]
+  },
 
   'bible plans': {
     love: [
@@ -37,5 +37,18 @@ const devotions = {
   }
 };
 
-console.log(devotions['bible verses'].love[0]);
-console.log(devotions['bible plans'].love[0]);
+// console.log(devotions['bible verses'].love[0]);
+// console.log(devotions['bible plans'].love[0]);
+
+const verseAndPlanGenerator = (emotion) => {
+  //randomize bible verse from object
+  const mood = emotion.toLowerCase();
+  if (mood === 'love') {
+    let loveVerse = devotions['bible verses'].love[Math.floor(Math.random() * devotions['bible verses'].love.length)];
+    let lovePlan = devotions['bible plans'].love[Math.floor(Math.random() * devotions['bible plans'].love.length)];
+    return `${mood.toUpperCase()}:\n ${loveVerse}\nPLAN: \n ${lovePlan}`
+
+  }
+
+}
+console.log(verseAndPlanGenerator('love'));
